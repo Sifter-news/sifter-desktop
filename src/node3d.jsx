ae6820de228c2c02c12a00b55822e1ef084c6694
John Ferreira <johnferreira@gmail.com>
2024-11-02 12:58:55 +0100

    folder updates
    Signed-off-by: John Ferreira <johnferreira@gmail.com>

---
+++ src/node3d.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from '@/components/ui/sonner';
+import { AuthProvider } from '@/components/AuthProvider';
+import LoginPage from '@/pages/LoginPage';
+import HomePage from '@/pages/HomePage';
+import ProjectView from '@/pages/ProjectView';
+import AccountPage from '@/pages/AccountPage';
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/project/:id" element={<ProjectView />} />
+            <Route path="/account" element={<AccountPage />} />
+          </Routes>
+          <Toaster />
+        </AuthProvider>
+      </Router>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
